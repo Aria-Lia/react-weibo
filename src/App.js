582f2516d74bfce73baf7519d2ca3c0fbc09b95c
Aria-Lia <42504328+Aria-Lia@users.noreply.github.com>
2019-04-26 19:15:51 +0800

    use react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import {Layout} from './containers/frame/index'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        123
+        <Layout />
       </div>
     );
   }
