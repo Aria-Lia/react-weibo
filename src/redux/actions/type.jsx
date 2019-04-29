38f75165073bc3eb79cc0e19d4a79fab5101123a
Aria-Lia <42504328+Aria-Lia@users.noreply.github.com>
2019-04-29 15:29:36 +0800

    redux actions

--- src/redux/actions/type.jsx
+++ src/redux/actions/type.jsx
@@ -1,5 +1,12 @@
 /**
  * Action Type
  */
+
+ /**
+  * Access Token (Async)
+  */
 export const GET_AUTH_CODE = 'GET_AUTH_CODE'
 export const GET_ACCESS_TOKEN = 'GET_ACCESS_TOKEN'
+export const GET_ACCESS_TOKEN = 'GET_ACCESS_TOKEN_SUCCESS'
+export const GET_ACCESS_TOKEN = 'GET_ACCESS_TOKEN_FAILURE'
+export const GET_ACCESS_TOKEN = 'GET_ACCESS_TOKEN_STARTED'
