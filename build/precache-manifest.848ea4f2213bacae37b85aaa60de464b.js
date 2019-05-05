572c635be217e73270b313579c6f3ba0632e3fc8
Aria-Lia <42504328+Aria-Lia@users.noreply.github.com>
2019-05-05 17:03:42 +0800

    test build

---
+++ build/precache-manifest.848ea4f2213bacae37b85aaa60de464b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "23d7c02f4974463c4b17e54806193f98",
+    "url": "/react-weibo/static/media/style.23d7c02f.less"
+  },
+  {
+    "revision": "a5babdcd0fab5ff4dab5868d4c2d5a2c",
+    "url": "/react-weibo/static/media/bg.a5babdcd.jpg"
+  },
+  {
+    "revision": "df99f953204fe13b632b",
+    "url": "/react-weibo/static/js/runtime~main.d33a56b4.js"
+  },
+  {
+    "revision": "5399a39ac45caff8e0e3",
+    "url": "/react-weibo/static/js/main.099b6c8f.chunk.js"
+  },
+  {
+    "revision": "d85937050390a4876a33",
+    "url": "/react-weibo/static/js/2.0dfbf095.chunk.js"
+  },
+  {
+    "revision": "5399a39ac45caff8e0e3",
+    "url": "/react-weibo/static/css/main.c0760eb2.chunk.css"
+  },
+  {
+    "revision": "d85937050390a4876a33",
+    "url": "/react-weibo/static/css/2.7782662f.chunk.css"
+  },
+  {
+    "revision": "8a42cc5a6dabb1bcfadb89dcd65ea7ed",
+    "url": "/react-weibo/index.html"
+  }
+];
